1ce544e2046dde3afb4a6f0f7484b63749029cf6
Dima <dimalebedev6902@gmail.com>
2023-02-06 00:03:39 +0200

    cors headers updated

--- index.js
+++ index.js
@@ -15,6 +15,20 @@ app.use(express.json());
 app.use(cors());
 app.use(xss());
 app.use(helmet());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", true);
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+});
 
 const start = async () => {
   try {
